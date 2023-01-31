c445263bb560de676f1dd5f6a4c22b4a462f5f19
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-01 00:01:34 +0100

    wavesurfer

--- JS/index.js
+++ JS/index.js
@@ -1,95 +1,4 @@
 
-            var player = document.getElementById("AudioPlayer");
- 
-            function play(isPlaying){
-              if(isPlaying){
-                player.play();
-                player.get
-                document.getElementById("playBtn").style.display = "none";
-                document.getElementById("pauseBtn").style.display = "block";
-              }else{
-                player.pause();
-                document.getElementById("pauseBtn").style.display = "none";
-                document.getElementById("playBtn").style.display = "block";
-              }
-              player.addEventListener("timeupdate", function() {
-              var currentTime = player.currentTime;
-              var duration = player.duration;
-              $('.hp_range').stop(true,true).animate({'width':(currentTime +.25)/duration*100+'%'},250,'linear');
-              });
-            }
-
-            $(".hp_slide").on("click", function(e) {
-              var progbar = $('.hp_slide');
-              var pos = e.pageX - progbar.offset().left; //Position cursor
-              var percent = pos / progbar.width(); //Get width element
-              player.currentTime = percent * player.duration;
-              progbar.value = percent / 100;
-            });
-
-            document.addEventListener("DOMContentLoaded", () => {
-            const range = document.querySelector(".volume input[type=range]");
-            const barHoverBox = document.querySelector(".volume .bar-hoverbox");
-            const fill = document.querySelector(".volume .bar .bar-fill");
-            const setValue = (value) => {
-              fill.style.width = value + "%";
-              range.setAttribute("value", value)
-              range.dispatchEvent(new Event("change"))
-            }
-            setValue(range.value);
-            const calculateFill = (e) => {
-              let offsetX = e.offsetX
-              if (e.type === "touchmove") {
-                offsetX = e.touches[0].pageX - e.touches[0].target.offsetLeft
-              }
-              const width = e.target.offsetWidth - 30;
-            
-              setValue(
-                Math.max(
-                  Math.min(
-                    (offsetX - 15) / width * 100.0,
-                    100.0
-                    ),0));
-            }
-            let barStillDown = false;
-            
-            barHoverBox.addEventListener("touchstart", (e) => {barStillDown = true;calculateFill(e);}, true);
-            barHoverBox.addEventListener("touchmove", (e) => {
-              if (barStillDown) {
-                calculateFill(e);
-              }
-            }, true);
-            barHoverBox.addEventListener("mousedown", (e) => {
-              barStillDown = true;
-              calculateFill(e);
-            }, true);
-            barHoverBox.addEventListener("mousemove", (e) => {
-              if (barStillDown) {
-                calculateFill(e);
-              }
-            });
-            barHoverBox.addEventListener("wheel", (e) => {
-              const newValue = +range.value + e.deltaY * 0.5;
-              setValue(Math.max(
-                Math.min(newValue,100.0),0))
-            });
-            document.addEventListener("mouseup", (e) => {
-              barStillDown = false;
-            }, true);
-            document.addEventListener("touchend", (e) => {
-              barStillDown = false;
-            }, true);
-          })
-
-
-          let volume = document.getElementById('vol-range');
-
-          volume.addEventListener("change", function(e) {
-            player.volume = e.currentTarget.value / 100;
-          })
-
-
-
           const links = document.querySelectorAll('#nav-tab-menu2> .nav-link');
             links.forEach((link) => {
                 link.addEventListener('click', (e) => {
@@ -115,26 +24,65 @@
                 document.getElementById("nav-songs").style.display="flex";
             }
         }   
-        
-        
-        function playPhantom(){
-            play(1);
-            document.getElementById("AudioPlayer").setAttribute("src","../MP3/ARTIST/JUSTICE/CROSS/phantompt2.mp3");
-            document.getElementById("NowPlaying").setAttribute("src","../IMAGES/ALBUMS/cross.jpg");
-            $("#ASongName").text("Phantom Pt.II");
-            $("#AArtistName").text("Justice");
-            player.play();
-        }
 
-        function playNightcall(){
-            play(1);
-            document.getElementById("AudioPlayer").setAttribute("src","../MP3/ARTIST/KAVINSKY/OUTRUN/nightcall.mp3");
-            document.getElementById("NowPlaying").setAttribute("src","../IMAGES/ALBUMS/OUTRUN.jpg");
-            $("#ASongName").text("Nightcall");
-            $("#AArtistName").text("Kavinsky");
-            player.play();
-        }
-
+        duration = document.querySelector("#duration");
+        current = document.querySelector("#current");
+        playPause = document.querySelector("#playPause");
+        
+        var timeCalculator = function (value) {
+            second = Math.floor(value % 60);
+            minute = Math.floor((value / 60) % 60);
+            
+            if (second < 10) {
+                second = "0" + second;
+            }
+        
+            return minute + ":" + second;
+        };
+        
+        //start wavesurfer object 
+        wavesurfer = WaveSurfer.create({
+            container: "#wave",
+            waveColor: "#cdedff",
+            progressColor: "#1AAFFF",
+            height: 48,
+            scrollParent: false
+        });
+        
+        //load audio file
+        wavesurfer.load("../MP3/ARTIST/KAVINSKY/OUTRUN/nightcall.mp3");
+        
+        //play and pause a player
+        playPause.addEventListener("click", function (e) {
+            wavesurfer.playPause();
+        });
+        
+        //load audio duration on load
+        wavesurfer.on("ready", function (e) {
+            duration.textContent = timeCalculator(wavesurfer.getDuration());
+        });
+        
+        //get updated current time on play
+        wavesurfer.on("audioprocess", function (e) {
+            current.textContent = timeCalculator(wavesurfer.getCurrentTime());
+        });
+        
+        //change play button to pause on plying
+        wavesurfer.on("play", function (e) {
+            playPause.classList.remove("fi-rr-play");
+            playPause.classList.add("fi-rr-pause");
+        });
+        
+        //change pause button to play on pause
+        wavesurfer.on("pause", function (e) {
+            playPause.classList.add("fi-rr-play");
+            playPause.classList.remove("fi-rr-pause");
+        });
+        
+        //update current time on seek
+        wavesurfer.on("seek", function (e) {
+            current.textContent = timeCalculator(wavesurfer.getCurrentTime());
+        });
 
         
         
