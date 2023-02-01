74c8422d45d6a25776f3f6d5231f524f71778c75
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-01 01:58:21 +0100

    style player

--- JS/index.js
+++ JS/index.js
@@ -147,6 +147,17 @@
             xhttp.send();
           }  
 
+        function loadArtist() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+               document.getElementById("main").innerHTML = this.responseText;
+              }
+            };
+            xhttp.open("GET", "../HTML/artist.html", true);
+            xhttp.send();
+          }   
+
         window.addEventListener("load", (event) => {
             loadIndex();
             //playNightcall();
